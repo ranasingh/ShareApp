df3f28d3d7aadfa4c1d4181266f1710dd73f2b90
rana ranjit singh <singh@iterate.no>
2014-03-03 12:11:02 +0100

    Added dummy article list from backend

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,29 @@
 package controllers;
 
+import models.Article;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        List<Article> articles = createDummyArticles();
+
+        return ok(index.render(articles));
+    }
+
+    private static List<Article> createDummyArticles() {
+        List<Article> dummyList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            dummyList.add(new Article("Heading", "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui."));
+        }
+
+        return dummyList;
     }
 
 }
---
+++ app/models/Article.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Article {
+
+    private String header;
+    private String body;
+    private String imageUrl;
+
+    public Article(String header, String body) {
+        this.header = header;
+        this.body = body;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
