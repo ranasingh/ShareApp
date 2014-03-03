dfd3d9be85730c6993398e43f92a43e45da51ad3
rana ranjit singh <singh@iterate.no>
2014-03-03 12:34:01 +0100

    added custom css and listing articles nicely on index page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,7 +20,7 @@ public class Application extends Controller {
         List<Article> dummyList = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            dummyList.add(new Article("Heading", "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui."));
+            dummyList.add(new Article("Heading", "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."));
         }
 
         return dummyList;
