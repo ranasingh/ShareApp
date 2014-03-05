3ff12bc7f2497b2131370c9d0d7edd8053c77b57
rana ranjit singh <singh@iterate.no>
2014-03-05 09:19:56 +0100

    Added article data and showing a list of all articles from database

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -13,6 +13,22 @@ CREATE TABLE User (
     created timestamp default current_timestamp
 );
 
+CREATE TABLE Article (
+  id bigserial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  producer varchar(255),
+  header varchar NOT NULL,
+  teaser varchar NOT NULL,
+  body varchar NOT NULL,
+  published bit NOT NULL,
+  image_url varchar,
+  owner_id bigint REFERENCES USER(id),
+  borrower_id bigint REFERENCES USER(id),
+  modified timestamp default current_timestamp,
+  created timestamp default current_timestamp
+)
+
 # --- !Downs
 
 DROP TABLE User;
+DROP TABLE article;
